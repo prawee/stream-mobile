04b0f7dece6cee0ae4fac8626c7bc2fda730a8ee
Prawee Wongsa <prawee@hotmail.com>
2024-05-30 02:26:58 +0700

    update ui of bootstrap

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useState } from 'react'
+import Spinner from 'react-native-loading-spinner-overlay';
 
 const Home = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
     return (
-        <View>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <Spinner
+                visible={loading}
+                textContent={'Loading...'}
+                textStyle={{ color: '#FFF' }}
+            />
             <Text>Home</Text>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
-export default Home
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+    }
+});
+
+export default Home;
