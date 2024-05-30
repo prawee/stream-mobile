4a534a54258b355bd76b490f4188d862c72a246f
Prawee Wongsa <prawee@hotmail.com>
2024-05-30 09:05:03 +0700

    update logic with screen between login or inside app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,49 @@
 import 'react-native-gesture-handler';
-import { Slot, Stack } from 'expo-router';
+import { Slot, Stack, useRouter, useSegments } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AuthProvider } from '@/context/AuthContext';
+import { AuthProvider, useAuth } from '@/context/AuthContext';
+import { useEffect, useState } from 'react';
+import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-native-sdk';
+import { OverlayProvider } from 'stream-chat-expo'
+import Toast from 'react-native-toast-message';
+
 const InitialLayout = () => {
+  const { authState, initialized } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+  const [client, setClient] = useState<StreamVideoClient | null>(null);
+
+  useEffect(() => {
+    if (!initialized) return;
+
+    const inAuthGroup = segments[0] === '(inside)';
+    // console.log('inAuthGroup ', inAuthGroup);
+    console.log('authState ', authState);
+
+    if (authState?.authenticated && inAuthGroup) {
+      router.replace('/(inside)');
+    } else if (!authState?.authenticated) {
+      router.replace('/');
+    }
+    
+  }, [authState, initialized]);
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+    {!client && (
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    )}
+    {client && (
+      <StreamVideo client={client}>
+        <OverlayProvider>
+          <Slot />
+          <Toast />
+        </OverlayProvider>
+      </StreamVideo>
+    )}
+    </>
   )
 }
 
